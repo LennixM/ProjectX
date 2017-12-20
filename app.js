466810241d68377b5aba732855b34109e66fb636
LenniXM <lennart_meyer@me.com>
2017-12-20 05:32:04 +0100

    added layout and two commits

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    title: 'Leja Hedgefund'
+  });
+});
+
+app.get('/positions/add', (req, res) => {
+  res.render('add_position', {
+    title: 'Add position'
+  })
 });
 
 app.listen(3000, () => {
